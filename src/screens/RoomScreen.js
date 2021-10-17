713bc2d90787a9e165ef10410af3b5a4370eaa7f
kanokphon2543 <85719250+kanokphon2543@users.noreply.github.com>
2021-10-17 19:25:53 +0700

    mai_wei_law_ka

---
+++ src/screens/RoomScreen.js
@@ -0,0 +1,247 @@
+import React, { useState, useContext, useEffect } from 'react';
+import {
+  GiftedChat,
+  Bubble,
+  Send,
+  Actions,
+  ActionsProps,
+  SystemMessage
+} from 'react-native-gifted-chat';
+import { ActivityIndicator, View, StyleSheet, image, Alert } from 'react-native';
+import { IconButton } from 'react-native-paper';
+import { AuthContext } from '../navigation/AuthProvider';
+import firestore from '@react-native-firebase/firestore';
+import useStatsBar from '../utils/useStatusBar';
+
+// import storage from '@react-native-firebase/storage';
+import * as ImagePicker from 'react-native-image-picker';
+// import * as Progress from 'react-native-progress';
+
+export default function RoomScreen({ route }) {
+  useStatsBar('light-content');
+
+  const [messages, setMessages] = useState([]);
+  const { thread } = route.params;
+  const { user } = useContext(AuthContext);
+  const currentUser = user.toJSON();
+  const [image, setImage] = useState([]);
+
+  async function handleSend(messages,image) {
+    const text = messages[0].text;
+    const img = image[0];
+
+    firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .add({
+        text,
+        // img,
+        createdAt: new Date().getTime(),
+        user: {
+          _id: currentUser.uid,
+          email: currentUser.email
+        }
+      });
+
+    await firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .set(
+        {
+          latestMessage: {
+            text,
+            // img,
+            createdAt: new Date().getTime()
+          }
+        },
+        { merge: true }
+      );
+  }
+
+  useEffect(() => {
+    const messagesListener = firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(querySnapshot => {
+        const messages = querySnapshot.docs.map(doc => {
+          const firebaseData = doc.data();
+
+          const data = {
+            _id: doc.id,
+            text: '',
+            // img: '',
+            createdAt: new Date().getTime(),
+            ...firebaseData
+          };
+
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.email
+            };
+          }
+
+          return data;
+        });
+
+        setMessages(messages);
+      });
+
+    // Stop listening for updates whenever the component unmounts
+    return () => messagesListener();
+  }, []);
+
+  function renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#6646ee'
+          }
+        }}
+        textStyle={{
+          right: {
+            color: '#fff'
+          }
+        }}
+      />
+    );
+  }
+
+  function renderLoading() {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color='#6646ee' />
+      </View>
+    );
+  }
+
+  function renderSend(props) {
+    return (
+      <Send {...props}>
+        <View style={styles.sendingContainer}>
+          <IconButton icon='send-circle' size={32} color='#6646ee' />
+        </View>
+      </Send>
+    );
+  }
+
+
+  // } https://github.com/FaridSafi/react-native-gifted-chat/issues/1745
+
+  function renderActions(props) {
+
+    const selectImage = () => {
+      const options = {
+        maxWidth: 2000,
+        maxHeight: 2000,
+        storageOptions: {
+          skipBackup: true,
+          path: 'images'
+        }
+      };
+
+      ImagePicker.launchImageLibrary(options, response => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        } else {
+          const source = { uri: response.assets[0].uri };
+          setImage(source);
+          return (source);
+          // setImage(source);
+        }
+      });
+    };
+
+    return (
+      <View style={styles.sendingContainer}>
+        <Actions
+          {...props}
+          options={{
+            ['Send Image']: selectImage,
+          }}
+          icon={() => (
+            <IconButton icon={'file-image'} size={25} color='#6646ee' />
+          )}
+          onSend={handleSend}
+        />
+      </View>
+
+    )
+  }
+
+  function scrollToBottomComponent() {
+    return (
+      <View style={styles.bottomComponentContainer}>
+        <IconButton icon='chevron-double-down' size={36} color='#6646ee' />
+      </View>
+    );
+  }
+
+  function renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        wrapperStyle={styles.systemMessageWrapper}
+        textStyle={styles.systemMessageText}
+      />
+    );
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+
+      onSend={handleSend}
+      user={{ _id: currentUser.uid }}
+      placeholder='Type your message here...'
+      alwaysShowSend
+      showUserAvatar
+      scrollToBottom
+      renderBubble={renderBubble}
+      renderLoading={renderLoading}
+
+      renderActions={renderActions}
+      // renderMessageImage={imageProps}
+      imageProps={image}
+
+      renderSend={renderSend}
+      scrollToBottomComponent={scrollToBottomComponent}
+      renderSystemMessage={renderSystemMessage}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sendingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottomComponentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  systemMessageWrapper: {
+    backgroundColor: '#6646ee',
+    borderRadius: 4,
+    padding: 5
+  },
+  systemMessageText: {
+    fontSize: 14,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+});
